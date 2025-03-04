f7f056013e35f822688e9298fd4c65edff936455
Sachintha Hasaranga <99173290+sachinthahasaranga@users.noreply.github.com>
2025-03-04 15:51:40 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
-//const API_BASE_URL = "https://book-review-application-sigma.vercel.app/api";
+//const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://book-review-application-sigma.vercel.app/api";
 
 
 // Fetch all reviews
